405bf47f95c8c49d3bfdb8ae6073f21f716c52d6
Theray070696 <Theray070696@gmail.com>
2019-09-26 15:27:02 -0400

    Initial commit

---
+++ ChronobaubleIsWhite/ChronobaubleIsWhite.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace ChronobaubleIsWhite
+{
+    [BepInPlugin("io.github.Theray070696.chronobaubleiswhite", "ChronobaubleIsWhite", "1.0.0")]
+    public class ChronobaubleIsWhite : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            ItemCatalog.GetItemDef(ItemIndex.SlowOnHit).tier = ItemTier.Tier1;
+        }
+    }
+}
